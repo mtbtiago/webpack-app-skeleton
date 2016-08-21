96906e7870362f6766cf4a3056a1f512df4bc05e
mtbtiago <tiago.ameller@gmail.com>
2016-08-21 18:14:35 +0200

    create more gulp tasks for dev and production

---
+++ gulpfile.babel.js
@@ -0,0 +1,40 @@
+import gulp from "gulp";
+import webpack from "webpack";
+import chalk from "chalk";
+import rimraf from "rimraf";
+import {create as createServerConfig} from "./webpack.server"
+
+const $ = require("gulp-load-plugins")();
+
+// -------------------
+// Public Tasks
+gulp.task("clean:server", cb => rimraf("./build", cb));
+gulp.task("clean:client", cb => rimraf("./public/build", cb));
+gulp.task("clean", gulp.parallel("clean:server", "clean:client"));
+gulp.task("dev:server", gulp.series("clean:server", devServerBuild));
+gulp.task("prod:server", gulp.series("clean:server", prodServerBuild));
+
+// -------------------
+// Private Server Tasks
+const devServerWebpack = webpack(createServerConfig(true));
+
+function devServerBuild(callback) {
+  devServerWebpack.run((error, stats) => {
+    outputWebpack("Dev:Server", error, stats);
+    callback();
+  });
+}
+
+function prodServerBuild(callback) {
+  const prodServerWebpack = webpack(createServerConfig(false));
+  prodServerWebpack.run((error, stats) => {
+    outputWebpack("Prod:Server", error, stats);
+    callback();
+  });
+}
+
+// ---------------------
+// Helpers
+function outputWebpack(label, error, stats) {
+  console.log(stats.toString());
+}
