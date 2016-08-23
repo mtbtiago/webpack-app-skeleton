c3fba0489a49dd65a80d0777eaa149aae456c5fd
mtbtiago <tiago.ameller@gmail.com>
2016-08-23 18:50:24 +0200

    finally we got an application skeleton

--- src/client/application.js
+++ src/client/application.js
@@ -1,11 +1,1 @@
-import $ from "jquery";
-import {blgegh} from "shared/test";
 import "./application.scss";
-
-blgegh();
-
-$("body").html("the new blegh is here");
-
-if (module.hot) {
-  module.hot.accept();
-}
--- src/shared/test.js
+++
@@ -1,3 +0,0 @@
-export function blgegh() {
-  console.log("It worked");
-}
